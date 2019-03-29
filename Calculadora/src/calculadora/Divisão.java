a9d0008c26eb960c6439f4d7f6633e04cc19d5ea
Daniel <revolverocelot.r3v0lv3r0c3l0t@gmail.com>
2019-03-28 23:29:27 -0300

    Funções de Multiplicação e Divisão

---
+++ Calculadora/src/calculadora/Divisão.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author danpg
+ */
+public class Divisão {
+    public double Divisao(int val1,int val2){
+        return val1/val2;
+    }
+}
